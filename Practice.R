1342ceb16c7c66bb10d0bd8035e20fa33c237ba7
Nikhat Bhayani <nikhataly@gmail.com>
2015-10-25 13:44:11 -0400

    removing inertial signals

--- Practice.R
+++ Practice.R
@@ -1,1 +1,6 @@
-baxt <- read.txt("./Dataset/test/Intertial Signal")
+
+subt <- read.table("./Dataset/test/subject_test.txt")
+
+Xt <- read.table("./Dataset/test/X_test.txt")
+
+Yt <- read.table("./Dataset/test/Y_test.txt")
